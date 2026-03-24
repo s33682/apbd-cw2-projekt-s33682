2ec5bfd294b9aa99bbcd8c37a2b466a2933b224c
s33682 <s33682@pjwstk.edu.pl>
2026-03-24 19:55:39 +0100

    Added ServiceRaport functionality

--- EquipmentRental/EquipmentRental/Service/ServiceRaport.cs
+++ EquipmentRental/EquipmentRental/Service/ServiceRaport.cs
@@ -8,4 +8,13 @@ public class ServiceRaport
     {
         this.db = db;
     }
+
+    public void PrintExpiredRentals()
+    {
+        List<Rental> rentals = db.GetExpiredRentals();
+        foreach (Rental rental in rentals)
+        {
+            Console.WriteLine($"{rental.Id}: {rental.Who.Username} rented {rental.Item.Name} ({rental.Item.Id}) on {rental.Date} (Delayed by {(DateTime.Now - rental.Date.AddDays(rental.Length)).Days} days)");
+        }
+    }
 }
